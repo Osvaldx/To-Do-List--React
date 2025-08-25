0fb0b88a4c34d47a692cedbb852c8f3ef99d67b3
osvaldx <romanonahuel24@gmail.com>
2025-08-25 10:22:16 -0300

    se agrego el llamado de toggleCompletada y en sus Props

--- src/components/ListaTareas.tsx
+++ src/components/ListaTareas.tsx
@@ -1,15 +1,18 @@
 import { Tarea } from "./Tarea"
 
+type typeTarea = { texto: string, completado: boolean }
+
 type Props = {
-    listaTareas: string[]
+    listaTareas: typeTarea[]
     borrarTarea: (index: number) => void
+    toggleCompletada: (index: number) => void
 }
 
-export const ListaTareas = ({listaTareas, borrarTarea}: Props) => {
+export const ListaTareas = ({listaTareas, borrarTarea, toggleCompletada}: Props) => {
     return (
         <div className="task-list">
             {listaTareas.map((tarea, index) => (
-                <Tarea key={index} tarea={tarea} borrarTarea={() => borrarTarea(index)}></Tarea>
+                <Tarea key={index} tarea={tarea} borrarTarea={() => borrarTarea(index)} toggleCompletada={() => toggleCompletada(index)}></Tarea>
             ))}
         </div>
     )
